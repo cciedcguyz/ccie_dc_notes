912901d169d8a9e3f45ab312fea82b4cb0095579
cciedcguyz <cciedcguyz@gmail.com>
2016-08-21 23:29:53 -0700

    adding binary search

--- binsearch.py
+++ binsearch.py
@@ -4,13 +4,19 @@
 
 
 def binsearch(lst,item):
-
     start = 0
-    end = len(lst) - 1
+    end = len(lst)
 
     while start <= end:
-        mid = len(lst) / 2
+        mid = (start + end) / 2
+        print 'item in lst[mid] is %d' % lst[mid]
         if item == lst[mid]:
-            print 'Your Number %d is present in the list in this index' % mid
-        elif item < lst[mid]
+            print 'Your Number %d is present in the list in this index %d' % (item,mid)
+            break
+        elif item < lst[mid]:
+            print 'Number is present in left side'
+            end = mid - 1
+        elif item > lst[mid]:
+            print 'Number is present in right side'
+            start = mid + 1
 
